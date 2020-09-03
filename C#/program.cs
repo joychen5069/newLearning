81431dc57247abfa41222100976b04bb370605e2
Joy Chen <joychen5069@gmail.com>
2020-09-03 13:27:15 -0400

    initializing C#

---
+++ C#/program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        statis void Main(string[] args)
+        {
+            
+        }
+    }
+}
