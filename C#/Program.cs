a2b56c69a8a02bc2b0f91c8875bc1c1e865c0364
Joy Chen <joychen5069@gmail.com>
2020-09-03 17:13:55 -0400

    udpated C#

---
+++ C#/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myInt = 5;
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(myInt);
+        }
+    }
+}
