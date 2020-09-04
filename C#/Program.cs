0fbf1b7c7e95bf79337c581d988dc4cef5a97c4a
Joy Chen <joychen5069@gmail.com>
2020-09-03 20:27:53 -0400

    done for the day

--- C#/Program.cs
+++ C#/Program.cs
@@ -6,9 +6,22 @@ namespace C_
     {
         static void Main(string[] args)
         {
-            int myInt = 5;
-            Console.WriteLine("Hello World!");
-            Console.WriteLine(myInt);
+            // int myInt = 5;
+            // Console.WriteLine("Hello World!");
+            // Console.WriteLine(myInt + " VALUE");
+            // Console.WriteLine("The value of myInt is {0}", myInt);
+
+
+            int x = 5;
+            x = x+1;
+
+            Console.WriteLine(x);
+
+            int y = 6;
+            y++;
+            Console.WriteLine(y);
+
+            
         }
     }
 }
