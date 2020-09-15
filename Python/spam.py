78321063eb46bbc06377650dad52de115546c33f
Joy Chen <joychen5069@gmail.com>
2020-09-15 15:57:44 -0400

    section 2

---
+++ Python/spam.py
@@ -0,0 +1,11 @@
+spam = 0
+while spam < 5:
+    print('Hello world')
+    spam = spam + 1
+
+
+name = ''
+while name != 'your name':
+    print('Please type your name')
+    name = input()
+print('Thank you')
