cc0a18a4c126c22152ca5eeb13625edd216fb195
Joy Chen <joychen5069@gmail.com>
2020-10-07 13:50:32 -0400

    figured out array manipulation

--- jsReview/arrayReview.js
+++ jsReview/arrayReview.js
@@ -1,7 +1,39 @@
 function arrayManipulation(n, queries) {
 
+    //take n and fill an array with n number of zeros
+    let array = new Array(n).fill(0);
+    console.log("array", array)
+    //then i would find the length of the queries and start from 0, and work my way up
+    //KEEP IN MIND, ITS A 1-INDEX ARRAY SO IT STARTS AT 1 NOT 0
+    for (let i = 0; i < queries.length; i++) {
+        //start of query
+        let start = queries[i][0];
+        console.log("start", start)
+        //end of query (ALL INCLUSIVE)
+        let end = queries[i][1];
+        
+        let numberToAdd = queries[i][2];
 
+        for (let j = 0; j < n; j++) {
+            
+            let index = j+1;
+            if(index >= start && index <=end){
+                array[j]+= numberToAdd
+            }
+        }
+        console.log(array)
+    }
+
+    //then after i finish all the queries, i would find the greatest value in the array and return it
+    console.log("MAX : ",Math.max(...array))
 }
+let a=5;
+let b = [
+    [1, 2, 100],
+    [2, 5, 100],
+    [3, 4, 100]]
+
+arrayManipulation(a, b)
 
 //sample input=
 //5 3
@@ -9,7 +41,7 @@ function arrayManipulation(n, queries) {
 // 2 5 100
 // 3 4 100
 
-//n = 3
+//n = 5 starting point would be [0, 0, 0, 0, 0]
 //queries = {[1, 2, 100], [2, 5, 100], [3, 4, 100]}
 
 //would be 
