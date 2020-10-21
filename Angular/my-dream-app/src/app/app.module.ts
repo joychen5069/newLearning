cbefeea140eefce7b84691b5eca580b7043b9483
Joy Chen <joychen5069@gmail.com>
2020-10-21 09:55:47 -0400

    added my own component

--- Angular/my-dream-app/src/app/app.module.ts
+++ Angular/my-dream-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { ExampleComponentComponent } from './example-component/example-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    ExampleComponentComponent
   ],
   imports: [
     BrowserModule,
