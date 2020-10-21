47d77a58fa557e3a2280f1b9777f6bec9aa66cd8
Joy Chen <joychen5069@gmail.com>
2020-10-21 14:24:56 -0400

    added all the components

--- Angular/recipe-app/src/app/app.module.ts
+++ Angular/recipe-app/src/app/app.module.ts
@@ -2,10 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { RecipesListComponent } from './recipe/recipes-list/recipes-list.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeItemsComponent } from './recipe/recipes-list/recipe-items/recipe-items.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component'
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    RecipesListComponent,
+    RecipeDetailsComponent,
+    ShoppingListComponent,
+    RecipeItemsComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
