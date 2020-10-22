6d72a782497b2bdd289877a4e84615167be7eb1f
Joy Chen <joychen5069@gmail.com>
2020-10-22 09:39:40 -0400

    added recipe component using string interpolation

--- Angular/recipe-app/src/app/recipe/recipes-list/recipes-list.component.ts
+++ Angular/recipe-app/src/app/recipe/recipes-list/recipes-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model'
+
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is simply a text', 'https://cdn.pixabay.com/photo/2014/12/21/23/28/recipe-575434_960_720.png')
+  ];
   constructor() { }
 
   ngOnInit(): void {
