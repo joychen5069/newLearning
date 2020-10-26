07b0f98ec5596073f75f9b0c78f73f7a9bd7c567
Joy Chen <joychen5069@gmail.com>
2020-10-26 14:01:44 -0400

    stuck on lecture 85. click button wont work

--- Angular/recipe-app/src/app/header/header.component.ts
+++ Angular/recipe-app/src/app/header/header.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core'
+import { Component, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent{
-    
+export class HeaderComponent {
+    featureSelected = new EventEmitter<string>();
+
+    onSelect(feature: string) {
+        console.log("onSelect clicked", feature)
+        this.featureSelected.emit(feature);
+    }
 }
