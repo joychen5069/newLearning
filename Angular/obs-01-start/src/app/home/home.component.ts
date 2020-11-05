88ee1116683103636af5f3899f33580236fcc644
Joy Chen <joychen5069@gmail.com>
2020-11-05 12:13:04 -0500

    finished section 13

--- Angular/obs-01-start/src/app/home/home.component.ts
+++ Angular/obs-01-start/src/app/home/home.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+
+    const customIntervalObservable = new Observable(observer => {
+      let count = 0
+      setInterval(() => {
+        observer.next(count);
+        if (count > 3) {
+          observer.error(new Error('count is greater than 3'))
+        }
+        count++
+      }, 1000)
+    });
+
+    customIntervalObservable.pipe(map(data =>{
+      return data
+    }))
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(data => {
+      console.log(data)
+
+    })
   }
 
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
