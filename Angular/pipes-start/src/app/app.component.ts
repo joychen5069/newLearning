fb2e60cb73ed050d62bb48de2d850d69bfa43d65
Joy Chen <joychen5069@gmail.com>
2020-11-11 13:29:52 -0500

    understanding asyn and and pure pipes

--- Angular/pipes-start/src/app/app.component.ts
+++ Angular/pipes-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { removeListener } from 'process';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  appStatus = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+      resolve('stable')
+    },2000)
+  })
   servers = [
     {
       instanceType: 'medium',
@@ -40,5 +46,13 @@ export class AppComponent {
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+  onAddServer(){
+    this.servers.push({
+      instanceType: 'small',
+      name: 'New Server',
+      status: 'stable',
+      started: new Date(15,1,2017)
+    })
   }
 }
