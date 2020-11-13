6627bb4328bef292483898a90b877e0d02aefe81
Joy Chen <joychen5069@gmail.com>
2020-11-13 12:52:32 -0500

    output posts

--- Angular/http-01-start/src/app/app.component.ts
+++ Angular/http-01-start/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators'
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +9,21 @@ import { map } from 'rxjs/operators'
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.fetchPosts()
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
-    this.http.post(
+    this.http.post<{ name: string }>(
       'https://udemy-learning-b5e36.firebaseio.com/posts.json', postData
-      ).subscribe(responseData =>{
-        console.log(responseData)
-      })
+    ).subscribe(responseData => {
+      console.log(responseData)
+    })
   }
 
   onFetchPosts() {
@@ -34,20 +35,20 @@ export class AppComponent implements OnInit {
     // Send Http request
   }
 
-  private fetchPosts(){
-    this.http.get(
+  private fetchPosts() {
+    this.http.get<{ [key: string]: Post }>(
       'https://udemy-learning-b5e36.firebaseio.com/posts.json'
-      ).pipe(map(responseData => {
-        const postsArray = [];
-        for( const key in responseData){
-          if(responseData.hasOwnProperty(key)){ 
-           postsArray.push({...responseData[key], id: key})
-          }
+    ).pipe(map(responseData => {
+      const postsArray: Post[] = [];
+      for (const key in responseData) {
+        if (responseData.hasOwnProperty(key)) {
+          postsArray.push({ ...responseData[key], id: key })
         }
-        return postsArray
-      }))
-      .subscribe(posts=>{
-        console.log(posts)
+      }
+      return postsArray
+    }))
+      .subscribe(posts => {
+        this.loadedPosts = posts
       })
   }
 }
