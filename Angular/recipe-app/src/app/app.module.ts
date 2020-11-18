2613870a6cc63824de77d42ae49e1d3058e468b6
Joy Chen <joychen5069@gmail.com>
2020-11-18 13:51:39 -0500

    created shared module

--- Angular/recipe-app/src/app/app.module.ts
+++ Angular/recipe-app/src/app/app.module.ts
@@ -5,27 +5,20 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { LoadingSpinnerComponent } from './shared/loadingSpinner/loading-spinner.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
-import { AlertComponent } from './shared/alert/alert.component';
-import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModules } from './shopping-list/shopping-list.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    DropdownDirective,
-    AuthComponent,
-    LoadingSpinnerComponent,
-    AlertComponent,
-    PlaceholderDirective,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
@@ -34,7 +27,8 @@ import { ShoppingListModules } from './shopping-list/shopping-list.module';
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
-    ShoppingListModules
+    ShoppingListModules,
+    SharedModule
   ],
   providers: [
     ShoppingListService,
@@ -45,7 +39,6 @@ import { ShoppingListModules } from './shopping-list/shopping-list.module';
       multi: true,
     },
   ],
-  bootstrap: [AppComponent],
-  entryComponents: [AlertComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
