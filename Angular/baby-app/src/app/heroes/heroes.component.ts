9b879bf5216cd477142e4119f44dfeda90af6afe
Joy Chen <joychen5069@gmail.com>
2021-01-11 13:32:55 -0500

    added new onclick and cycled through full list

--- Angular/baby-app/src/app/heroes/heroes.component.ts
+++ Angular/baby-app/src/app/heroes/heroes.component.ts
@@ -1,20 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm',
   };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  selectedHero: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
